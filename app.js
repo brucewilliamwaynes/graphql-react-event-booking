d834c6c02a3371b80ed74449c553b79a32502645
Anirban Mukherjee <anirban.mukherjee@jda.com>
2019-05-10 20:04:15 +0530

    Established Relations and created User model

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
 app.use(bodyParser.json());
-/*
-app.get('/', (req, res, next) => {
-    res.send("Hello ! I am Batman!");
-});
-*/
 
 app.use('/graphql', 
     graphqlHttp( {
@@ -27,11 +24,22 @@ app.use('/graphql',
                 date : String!
             }
 
+            type User {
+                _id : ID!
+                email : String!
+                password : String
+            }
+
             input EventInput {
                 title : String!
                 description : String!
                 price : Float!
                 date : String!
+            }
+
+            input UserInput {
+                email : String!
+                password : String!
             }
 
             type RootQuery {
@@ -40,6 +48,7 @@ app.use('/graphql',
 
             type RootMutation {
                 createEvent(eventInput : EventInput): Event
+                createUser(userInput : UserInput) : User
             }
 
             schema {
@@ -49,7 +58,6 @@ app.use('/graphql',
         `),
         rootValue : {
             events : () => {
-               // return ['Batman','SuperMan','Aquaman','WonderWoman'];
                 return Event
                 .find()
                 .then( events => {
@@ -67,18 +75,58 @@ app.use('/graphql',
                     title : args.eventInput.title,
                     description : args.eventInput.description,
                     price : +args.eventInput.price,
-                    date : new Date(args.eventInput.date)
+                    date : new Date(args.eventInput.date),
+                    creator : 'userID from MongoDB'
                 });
-               return event
+                let createdEvent;
+                return event
                 .save()
                 .then(result => {
-                   console.log(result);
+                    createdEvent = { ...result._doc, _id: result._doc._id.toString()};
+                    return User.findById('userID by MONGODB')
+                    console.log(result);
                     return { ...result._doc, _id: result.id};
-                }).catch(err => {
+                })
+                .then(user => {
+                    if(!user) {
+                        throw new Error('User not found.');
+                    }
+                    user.createdEvent.push(event);
+                    return user.save();
+                })
+                .then(result => {
+                    return createdEvent;
+                })
+                .catch(err => {
                    console.log(err);
                    throw err;
                 });
-            }
+            },
+            createUser : args => {
+                return User
+                .findOne({
+                    email: args.userInput.email
+                })
+                .then(user => {
+                    if(user) {
+                        throw new Error('User exists already.');
+                    }
+                    return bcrypt.hash(args.userInput.password,12);
+                })
+                .then(hashedPassword => {
+                    const user = new User({
+                        email : args.userInput.email,
+                        password : hashedPassword
+                    }); 
+                    return user.save();
+                })
+                .then( result => {
+                    return { ...result._doc, password : null, _id: result.id};
+                })
+                .catch(err => {
+                    throw err;
+                });
+            } 
         },
         graphiql : true
     })
