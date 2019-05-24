2b5609bf9b919d1b7043647d662a03c743f990bc
Anirban Mukherjee <anirban.mukherjee@jda.com>
2019-05-24 15:21:54 +0530

    FrontEnd React setup Routing complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React , { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
+import AuthPage from './pages/Auth';
 import './App.css';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>It Works!</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+      return (
+        <BrowserRouter>
+          <Switch>
+            <Redirect from ='/' to = "/auth" exact />
+            <Route path ='/auth' component = { AuthPage } />
+            <Route path ='/events' component = { EventsPage } />
+            <Route path ='/bookings' component = { BookingsPage } />
+          </Switch>
+        </BrowserRouter>
+      );
+    }
 }
 
 export default App;
