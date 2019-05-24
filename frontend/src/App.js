b4b8eb79340cb63e07628b9d4d770ab09f454430
Anirban Mukherjee <anirban.mukherjee@jda.com>
2019-05-24 20:23:29 +0530

    Adding Navigation Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,27 @@ import React , { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
-import './App.css';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './Components/Navigation/MainNavigation';
+
+import './App.css';
 
 class App extends Component {
     render() {
       return (
         <BrowserRouter>
-          <Switch>
-            <Redirect from ='/' to = "/auth" exact />
-            <Route path ='/auth' component = { AuthPage } />
-            <Route path ='/events' component = { EventsPage } />
-            <Route path ='/bookings' component = { BookingsPage } />
-          </Switch>
+          <React.Fragment>
+            <MainNavigation />
+            <main className = "main-content">
+            <Switch>
+              <Redirect from ='/' to = "/auth" exact />
+              <Route path ='/auth' component = { AuthPage } />
+              <Route path ='/events' component = { EventsPage } />
+              <Route path ='/bookings' component = { BookingsPage } />
+            </Switch>
+            </main>
+          </React.Fragment>
         </BrowserRouter>
       );
     }
